10dfe4e1aae28d8887bc1676cb7a9c9f6332b986
NikolaosKalfas <nikolaoskalfas1989@gmail.com>
2022-10-15 18:03:40 +0100

    added taliwind css

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,19 +3,25 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Portfolio React`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "TF15G-KC1LGAeIm4bXEkPCDJNE8XOJo1I4UQlUToJ1Q",
-      "spaceId": "8m6w4lfavjb3"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "TF15G-KC1LGAeIm4bXEkPCDJNE8XOJo1I4UQlUToJ1Q",
+        spaceId: "8m6w4lfavjb3",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+  ],
 };
 
 export default config;
