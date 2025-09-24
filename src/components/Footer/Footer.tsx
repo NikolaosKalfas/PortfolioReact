a5860eddd2ed81c60e08250d35923d5c390828ae
NikolaosKalfas <nikolaoskalfas1989@gmail.com>
2025-09-24 21:35:01 +0100

    phase-2: mobile layout tidy up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
   return (
     <footer className="border-t border-t-primary-color bg-off-white w-full">
       <div className="page-container">
-        <div className="text-navigation-color flex flex-col md:flex-row justify-between md:items-end font-semibold">
+        <div className="text-navigation-color flex flex-row justify-between md:items-end font-semibold">
           Nikolaos Kalfas &copy; {new Date().getFullYear()}
           <div className="">
             {footerData.socialLink &&
