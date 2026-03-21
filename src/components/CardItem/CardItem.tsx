cdf3f9a85619a68739b2b651012a21e4b625aa3d
Nikos Kalfas <nikos.kalfas@pulselive.com>
2026-03-21 19:19:04 +0000

    130 phase 2 - card item component

---
+++ src/components/CardItem/CardItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export type CardItemDataType = {
+    title: string,
+    description: {
+        description: string
+    }
+}
+
+const CardItem = ({title, description}: CardItemDataType) => {
+
+  return (
+    <article className='flex flex-col gap-2 lg:gap-4 card-item'>
+        <h3 className='font-bold lg:min-h-[70px] 2xl:min-h-[40px] mb-0'>{title}</h3>
+        <p>{description.description}</p>
+    </article>
+  )
+}
+
+export default CardItem
